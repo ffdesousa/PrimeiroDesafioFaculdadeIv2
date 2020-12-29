28192a0bab45f95f40e1efe182be57e71a2e2de2
Fernando Fernandes <contato@fernando-fernandes.com>
2020-12-29 01:14:56 -0400

    First Commit

---
+++ statics/js/desafio.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+    
+    const link = document.querySelector('#link');
+    
+    link.addEventListener('click', function(){
+        alert('Desafio Falculdade IV2');
+    });
+
+});
